aab19ca18a6c3440b4aef884d9b058715b799528
artursem <artur.semeniuk@gmail.com>
2021-05-27 20:03:33 +0200

    fix regex

--- validation.js
+++ validation.js
@@ -7,9 +7,9 @@ btn.addEventListener('click', (e) => {
     e.preventDefault();
 
     console.log(emailInput.value);
-    const regex = new RegExp('/\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi');
+    const regex = new RegExp(/\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi);
     const test = regex.test(emailInput.value);
     console.log(test);
 });
 
-// /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi'
+// /\b[\w\.-]+@[\w\.-]+\.\w{2,4QW}\b/gi
