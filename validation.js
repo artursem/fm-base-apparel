210e867761d90a6e50e7e38682d744a7355ed1ec
artursem <artur.semeniuk@gmail.com>
2021-05-27 19:56:24 +0200

    add regex

---
+++ validation.js
@@ -0,0 +1,15 @@
+const emailInput = document.querySelector('input');
+const btn = document.querySelector('button');
+
+
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    console.log(emailInput.value);
+    const regex = new RegExp('/\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi');
+    const test = regex.test(emailInput.value);
+    console.log(test);
+});
+
+// /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi'
