cfb2b905a03b020f5ba4652a61a7a8f28c20622c
artursem <artur.semeniuk@gmail.com>
2021-05-29 16:21:02 +0200

    add email validation

--- validation.js
+++ validation.js
@@ -1,15 +1,26 @@
 const emailInput = document.querySelector('input');
 const btn = document.querySelector('button');
+const errorMessage = document.querySelector('#error-msg');
 
+const showValid = (test) => {
+    if (test === true) {
+        emailInput.classList.remove('input-error');
+        emailInput.style.borderColor = '';
+        errorMessage.style.display = 'none';
+    } else {
+        emailInput.classList.add('input-error');
+        emailInput.style.borderColor = 'var(--sred)';
+        errorMessage.style.display = 'block';
+    }
+}
 
 
 btn.addEventListener('click', (e) => {
     e.preventDefault();
 
-    console.log(emailInput.value);
     const regex = new RegExp(/\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi);
     const test = regex.test(emailInput.value);
-    console.log(test);
+    showValid(test);
 });
 
 // /\b[\w\.-]+@[\w\.-]+\.\w{2,4QW}\b/gi
